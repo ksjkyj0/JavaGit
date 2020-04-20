08f557ee0d734c7108c4b1cef8f5da7e9f16af95
305 <305@305-PC>
2020-04-20 11:35:26 +0900

    first

---
+++ Ex01.java/src/Ex01.java
@@ -0,0 +1,16 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+		
+		if(n1 == n2) {
+			System.out.println("n1과 n2는 같다");
+		}
+		else {
+			System.out.println("n1과 n2는 같지않다.");
+		}
+	}
+	
+}
---
+++ Ex01.java/src/Ex03.java
@@ -0,0 +1,12 @@
+
+public class Ex03 {
+	public static void main(String[] args) {
+		int a =10;
+		int b =20;
+		String c ="";
+		
+		c = a>b? "a°¡ Å®´Ï´Ù":"b°¡ Å®´Ï´Ù";
+		
+		System.out.println("c = "+c);
+	}
+}
