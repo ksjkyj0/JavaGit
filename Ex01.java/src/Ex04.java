996a991fe2683a3d89e5bcc43c218d2a50fcb2ff
305 <305@305-PC>
2020-04-20 12:02:40 +0900

    cha

--- Ex01.java/src/Ex04.java
+++ Ex01.java/src/Ex04.java
@@ -1,4 +1,25 @@
+import java.util.Scanner;
 
 public class Ex04 {
+	public static void main(String[] args) {
+		System.out.println("스위치 공부해야됨 ㅇㅅㅇ");
 
+		Scanner scan = new Scanner(System.in);
+
+		int input = scan.nextInt();
+		switch (input) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 2:
+			System.out.println("2");
+			break;
+		case 3:
+			System.out.println("3");
+			break;
+		default:
+			System.out.println("멍청이");
+			break;
+		}
+	}
 }
